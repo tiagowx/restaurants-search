75e0052b853fcde00b00e6e2aaf4495f00152f99
wxtiago <tiagobatista.tec@gmail.com>
2021-11-10 07:14:22 -0300

    Criação do Carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,54 @@
 import React, { useState } from "react";
 import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg'
+import restaurante from '../../assets/restaurante-fake.png';
 
-import { Container, Search } from "./styles";
-
+import { Carousel, CarouselTitle, Container, Logo, Map, Search, Wrapper } from "./styles";
+import { Card } from '../../components'
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
 
-  return (<Container>
-    <Search>
-      <img src={logo} alt="Logo do restaurante" />
-      <TextField
-        label='Pesquisar'
-        outlined
-      //onTrailingIconSelect={() => this.setState({ value: '' })}
-      //trailingIcon={<MaterialIcon role="button" icon="delete" />}
-      ><Input
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-      </TextField>
-    </Search>
-  </Container>
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do restaurante" />
+          <TextField
+            label='Pesquisar restaurante'
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search" />}
+          ><Input
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </TextField>
+          <CarouselTitle>Na Sua Area</CarouselTitle>
+          <Carousel {...settings}>
+            <Card photo={restaurante} title="Nome Restaurant"/>
+            <Card photo={restaurante} title="Nome Restaurant"/>
+            <Card photo={restaurante} title="Nome Restaurant"/>
+            <Card photo={restaurante} title="Nome Restaurant"/>
+            <Card photo={restaurante} title="Nome Restaurant"/>
+            <Card photo={restaurante} title="Nome Restaurant"/>
+            <Card photo={restaurante} title="Nome Restaurant"/>
+            <Card photo={restaurante} title="Nome Restaurant"/>
+          </Carousel>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
   );
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,10 @@
+import Slider from "react-slick";
 import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 export const Container = styled.aside`
   background-color:${(props) => props.theme.colors.background};
@@ -13,4 +19,27 @@ export const Search = styled.section`
     justify-content: center;
     background-color: #ffffff;
     padding: 16px;
+`;
+export const Logo = styled.img`
+  margin-bottom: 16px;
+`;
+
+export const Map = styled.div`
+  background-color: red;
+  width: 500px;
+`;
+
+export const Carousel = styled(Slider)`
+  .slick-slide {
+    margin-right: 16px;
+  }
+`;
+
+export const CarouselTitle = styled.h1`
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 29px;
+  margin: 16px 0;
 `;
